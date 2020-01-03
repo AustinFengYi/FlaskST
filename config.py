18cb6739844d930c4a14b89033e97563662e2bad
xs1523c <xs1523c@gmail.com>
2020-01-03 19:30:05 +0800

    CES_Adjustment

--- config.py
+++ config.py
@@ -5,14 +5,14 @@ class EnvironmentConfig():
     port = 7789
     df_his_record_len = 200
     max_thresholds = 5 
-    server_ip = 'https://test.iottalk.tw/'
+    server_ip = 'http://140.113.199.183:9999'
     mac_addr = 'ThreeST'
     ctlboard_profile = {
-        'd_name': 'ToggleRC',
-        'dm_name': 'ThreeST',
+        'd_name': 'CES_Panel',
+        'dm_name': 'Toggle',
         'u_name': 'yb',
         'is_sim': False,
-        'df_list': ['toggle'],
+        'df_list': ['toggle1','toggle2','toggle3','toggle4','toggle5'],
     } 
 
 env_config = EnvironmentConfig() 
--- static/main.js
+++ static/main.js
@@ -1,8 +1,44 @@
 // toggle
 $(document).ready(function(){
   $("#toggle-button1").addClass("active");
-  $(".tri-state-toggle-button").click(function(){
-    $(".tri-state-toggle-button").removeClass("active");
+  $(".tri-state-toggle-button1").click(function(){
+    $(".tri-state-toggle-button1").removeClass("active");
+    var id = $(this).attr('id');
+    $("#" + id).addClass("active");
+  });
+});
+
+$(document).ready(function(){
+  $("#toggle-button4").addClass("active");
+  $(".tri-state-toggle-button2").click(function(){
+    $(".tri-state-toggle-button2").removeClass("active");
+    var id = $(this).attr('id');
+    $("#" + id).addClass("active");
+  });
+});
+
+$(document).ready(function(){
+  $("#toggle-button7").addClass("active");
+  $(".tri-state-toggle-button3").click(function(){
+    $(".tri-state-toggle-button3").removeClass("active");
+    var id = $(this).attr('id');
+    $("#" + id).addClass("active");
+  });
+});
+
+$(document).ready(function(){
+  $("#toggle-button10").addClass("active");
+  $(".tri-state-toggle-button4").click(function(){
+    $(".tri-state-toggle-button4").removeClass("active");
+    var id = $(this).attr('id');
+    $("#" + id).addClass("active");
+  });
+});
+
+$(document).ready(function(){
+  $("#toggle-button13").addClass("active");
+  $(".tri-state-toggle-button5").click(function(){
+    $(".tri-state-toggle-button5").removeClass("active");
     var id = $(this).attr('id');
     $("#" + id).addClass("active");
   });
